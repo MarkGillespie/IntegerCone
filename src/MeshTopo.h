ab9d5e04d349ee9c4095c75883ddb2944beda076
Mark Gillespie <mark.gillespie81@gmail.com>
2022-12-07 08:56:00 -0500

    Switch back to system OpenMesh

--- src/MeshTopo.h
+++ src/MeshTopo.h
@@ -1,14 +1,13 @@
 #pragma once
 #include "MeshDefinition.h"
 
-struct MeshTopo
-{
-	int vN, fN, hN;
-	std::vector<bool> vb;
-	std::vector<int> h2f, h2v;
-	std::vector<OpenMesh::Vec3i> f2v, f2f, f2h;
+struct MeshTopo {
+  int vN, fN, hN;
+  std::vector<bool> vb;
+  std::vector<int> h2f, h2v;
+  std::vector<OpenMesh::Vec3i> f2v, f2f, f2h;
 
-	MeshTopo(const Mesh& mesh);
+  MeshTopo(const Mesh &mesh);
 };
 
-void phfToE(const MeshTopo& topo, std::vector<OpenMesh::Vec3i>& phf2e);
+void phfToE(const MeshTopo &topo, std::vector<OpenMesh::Vec3i> &phf2e);
