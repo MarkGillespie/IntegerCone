e0d00467b1a7783ba5a9a550951031cbfc72d8fe
Mark Gillespie <mark.gillespie81@gmail.com>
2022-12-07 09:42:36 -0500

    formatting

--- include/ConesFlattening.h
+++ include/ConesFlattening.h
@@ -1,11 +1,8 @@
-#include "Opt/Types.h"
 #include "MeshDefinition.h"
+#include "Opt/Types.h"
 
-namespace ConesFlattening
-{
-	void initCoef(const Mesh& mesh, int lp_, double sigma_);
-	void geneCone(VectorX& conesK, VectorX& phi, VectorX& MA, int coneMax, double& factor, int& iter, double& gamma_);
-}
-
-
-
+namespace ConesFlattening {
+void initCoef(const Mesh &mesh, int lp_, double sigma_);
+void geneCone(VectorX &conesK, VectorX &phi, VectorX &MA, int coneMax,
+              double &factor, int &iter, double &gamma_);
+} // namespace ConesFlattening
